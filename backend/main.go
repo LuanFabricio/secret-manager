27bb1235daa92fe9f76b81a1cc01a97f32dcc27a
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-04 16:56:02 -0300

    feat: Adds dotenv load before run the server

--- backend/main.go
+++ backend/main.go
@@ -7,11 +7,14 @@ import "net/http"
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"secret-manager/backend/controllers"
 )
 
 func main() {
+	godotenv.Load();
+
 	r := gin.Default();
 	r.GET("/ping", func (ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
