99f5be7e88373e8564dcedbc3da40b5e45f2eb8a
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-11 14:07:24 -0300

    test: Adds test for FindByUsername method

--- backend/tests/user_test.go
+++ backend/tests/user_test.go
@@ -99,3 +99,38 @@ func TestFindByUserID(t *testing.T) {
 		t.Fatalf("User CreatedAt not match")
 	}
 }
+
+func TestFindByUsername(t *testing.T) {
+	setupTest(t)
+
+	db := database.GetConnection()
+	new_user, err := user_model.Create(db, "test_username", "somepass")
+	if err != nil {
+		t.Fatalf("Test user create fail. Error: %v", err)
+	}
+
+	find_user, err := user_model.FindByUsername(db, new_user.Username)
+	if err != nil {
+		t.Fatalf("Test user find by id fail. Error: %v", err)
+	}
+
+	if *new_user.ID != *find_user.ID {
+		t.Fatalf("User ID not match.")
+	}
+
+	if new_user.Username != find_user.Username {
+		t.Fatalf("User Username not match")
+	}
+
+	if new_user.Hash != find_user.Hash {
+		t.Fatalf("User Hash not match")
+	}
+
+	if new_user.Active != find_user.Active {
+		t.Fatalf("User Active not match")
+	}
+
+	if !new_user.CreatedAt.Equal(find_user.CreatedAt){
+		t.Fatalf("User CreatedAt not match")
+	}
+}
