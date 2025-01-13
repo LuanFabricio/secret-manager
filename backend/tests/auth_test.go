670c3bab8638174ebb51ee03c246f6fd9425f9ba
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-12 23:05:24 -0300

    test: Adds ValidateToken test

--- backend/tests/auth_test.go
+++ backend/tests/auth_test.go
@@ -18,3 +18,20 @@ func TestGenerateToken(t *testing.T) {
 		t.Fatalf("Invalid token")
 	}
 }
+
+func TestValidateToken(t *testing.T) {
+	var id uint = 42
+	token, err := auth.GenerateToken(id)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !auth.ValidateToken(token) {
+		t.Fatalf("Invalid token, should be valid")
+	}
+
+	if auth.ValidateToken("wrong token") {
+		t.Fatalf("Valid token, should be invalid")
+	}
+}
