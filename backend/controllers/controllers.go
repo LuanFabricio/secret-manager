e523b14f00867453fe4a08e2df249c9edb3f741b
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-13 23:05:31 -0300

    feat: Adds AuthMiddleware on user endpoints

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -3,11 +3,17 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 
+	auth_middleware "secret-manager/backend/middlewares/auth"
 	"secret-manager/backend/controllers/user"
 )
 
 func BindRoutes(r *gin.Engine) {
-	r.GET("/user/id/:id", user.GetUserById);
-	r.GET("/user/username/:username", user.GetUserByUsername);
-	r.POST("/user", user.CreateUser);
+
+	authorized := r.Group("/")
+	authorized.Use(auth_middleware.AuthMiddleware())
+	{
+		authorized.GET("/user/id/:id", user.GetUserById);
+		authorized.GET("/user/username/:username", user.GetUserByUsername);
+		authorized.POST("/user", user.CreateUser);
+	}
 }
