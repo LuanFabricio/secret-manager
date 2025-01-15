10a948aee864e47ec3065e91a69a15546f68d129
LuanFabricio <luanlimaleite@hotmail.com>
2025-01-14 21:22:53 -0300

    feat: Adds auth route

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 
-	auth_middleware "secret-manager/backend/middlewares/auth"
 	"secret-manager/backend/controllers/user"
+	auth_controller "secret-manager/backend/controllers/auth"
+	auth_middleware "secret-manager/backend/middlewares/auth"
 )
 
 func BindRoutes(r *gin.Engine) {
+	r.POST("/auth", auth_controller.AuthCredentials)
 
 	authorized := r.Group("/")
 	authorized.Use(auth_middleware.AuthMiddleware())
